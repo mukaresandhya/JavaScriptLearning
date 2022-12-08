8134815b53f3a166fb9d3ac54ff9590c38fa3a61
mukaresandhya <mukaresandhya@gmail.com>
2022-12-08 12:03:21 +0530

    Create 01_variable_assign.js file Variable Declaration and Initialization
    Create 01_variable_assign.js file Variable Declaration and Initializatio

---
+++ 01_variable_assign.js
@@ -0,0 +1,21 @@
+var myFullName;
+console.log(myFullName);
+
+var myFullName="sandhya Mukare"
+console.log(myFullName);
+
+var myFullName="Sandhya Suryakant Mukare";
+console.log(myFullName);
+
+var myLove="I love only JS";
+console.log(myLove);
+
+var firstName="Sandhya";
+var lastName="Mukare";
+var age=27;
+var address="Near Shivaji College Barshi";
+var pin_code=413401;
+var gender="Female";
+var is_married="Yes";
+
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
