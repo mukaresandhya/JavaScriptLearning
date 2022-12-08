a9393d50433c38cba508fe06eee33566283220e3
mukaresandhya <mukaresandhya@gmail.com>
2022-12-08 11:59:56 +0530

    Create 01_variable.js file variable declaration and initilization
    Create 01_variable.js file variable declaration and initilization

---
+++ 01_variable.js
@@ -0,0 +1,8 @@
+var myName; //Variable Declaration
+myName="Sandhya"; //Variable Initilization
+console.log("Sandhya");
+
+var myFrndName="Akanksha" //Variable Declaration on same line
+console.log(myFrndName);
+
+
