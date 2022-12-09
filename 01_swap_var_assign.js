d6b73245c21a8ca3c0171fb4a6962345cdfeef32
mukaresandhya <mukaresandhya@gmail.com>
2022-12-09 14:57:01 +0530

    Create 01_swap_var_assign.js
    Create 01_swap_var_assign.js

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,46 @@
+console.log("============================== Part 1 ==============================");
+var myNumber=100;
+var yourNumber=300;
+
+console.log("------------------------- Before Swapping -------------------------");
+console.log(myNumber,yourNumber);
+
+ var temp=100;
+ var myNumber=300;
+ var yourNumber=100;
+
+ console.log("------------------------- After Swapping -------------------------");
+ console.log(myNumber,yourNumber);
+
+
+
+console.log("============================== Part 2 ==============================");
+var sweety="Sweety";
+var cutie="Cutie";
+console.log("------------------------- Before Swapping -------------------------");
+console.log(sweety,cutie);
+
+var temp="Sweety";
+var sweety="Cutie";
+var cutie="Sweety";
+
+console.log("------------------------- After Swapping -------------------------");
+console.log(sweety,cutie);
+
+
+
+console.log("============================== Part 3 ==============================");
+
+var num1=100;
+var num2=200;
+var num3=300;
+
+console.log("------------------------- Before Swapping -------------------------");
+console.log(num1,num2,num3);
+
+var temp=100;
+var num1=200;
+var num2=300;
+var num3=100;
+console.log("------------------------- After Swapping -------------------------");
+console.log(num1,num2,num3);
