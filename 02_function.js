f49b55d011aea441650f42d119e94a7b23d864d4
mukaresandhya <mukaresandhya@gmail.com>
2022-12-10 13:41:44 +0530

    Create 02_function.js file
    Create 02_function.js file

---
+++ 02_function.js
@@ -0,0 +1,19 @@
+function show(){
+    console.log("Shri Siddheshwar Women's Polytechnic,Solapur");
+}
+show();
+
+
+
+function swapValues(value1,value2){
+    console.log("Before Swap :",value1,value2);
+    var temp=value1;
+    var value1=value2;
+    var value2=temp;
+    console.log("After Swap :",value1,value2);
+}
+
+swapValues(100,200);
+swapValues(2,4);
+swapValues("Sandhya","Uttam");
+swapValues("You","Me");
