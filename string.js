bb6bb851cd84201aaa7977e5deb094a78bf4e0a3
mukaresandhya <mukaresandhya@gmail.com>
2022-12-12 22:19:52 +0530

    Create string.js file
    Create string.js file

---
+++ string.js
@@ -0,0 +1,6 @@
+var greet="Good Morning";
+console.log("Given String is :",greet);
+console.log("Total Characters in this String :",greet.length);
+console.log("Charat index 3 :",greet.charAt(3));
+var totalLength=greet.length;
+console.log("Character At last index :",greet.charAt(totalLength-1));
