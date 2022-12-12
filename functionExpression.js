2b9510b1c9657791048a1df8b10f10d24bb1f3d3
mukaresandhya <mukaresandhya@gmail.com>
2022-12-12 22:18:57 +0530

    Create functionExpression.js file
    Create functionExpression.js file

---
+++ functionExpression.js
@@ -0,0 +1,29 @@
+var substraction =function (num1,num2){
+    var result=num1-num2;
+    console.log("Substraction :",result);
+    return result;
+        
+    }
+    var finalResult=substraction(10,5);
+    console.log(finalResult)
+
+
+    var myName = "Mohit";
+console.log(myName);
+
+// Function Expression
+var showResult = function() {
+    console.log("show.....");
+};
+showResult();
+
+// We can store a function as value in JS
+var substraction = function(num1, num2){
+    var result = num1 - num2;
+    console.log("substraction.......", result);
+    return result;
+}
+var result = substraction(45, 20);
+console.log(result);
+
+    
