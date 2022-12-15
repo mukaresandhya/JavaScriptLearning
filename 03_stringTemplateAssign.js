ff1de1bfdfcf05f5fbb258f87d2531e19280e494
mukaresandhya <mukaresandhya@gmail.com>
2022-12-15 16:30:30 +0530

    Create 03_stringTemplateAssign.js File
    Create 03_stringTemplateAssign.js File

---
+++ 03_stringTemplateAssign.js
@@ -0,0 +1,13 @@
+console.log(`My Dream Company is :"Amdocs"`);
+
+console.log(`------------------------------------------------`);
+
+var yourHobby1="Reading";
+var yourHobby2="Cooking";
+var yourHobby3="Travelling";
+
+console.log(`(Using Variable Substituation) My Hobbies Are : ${yourHobby1}  ${yourHobby2}  ${yourHobby3}`);
+
+console.log(`------------------------------------------------`);
+
+console.log(`(Using String String Template) My Hobbies Are : ${yourHobby1}${yourHobby2}${yourHobby3}`);
