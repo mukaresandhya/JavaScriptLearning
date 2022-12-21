2f6939386481c51dabb7b502bb8ecc42ef1e4b74
mukaresandhya <mukaresandhya@gmail.com>
2022-12-21 12:06:37 +0530

    Create 05_doWhileLoop.js file
    Create 05_doWhileLoop.js file

---
+++ 05_doWhileLoop.js
@@ -0,0 +1,72 @@
+// Print numbers from 0 to 10
+// initialization 0   condition <=10   update expre = ++
+var index =0;
+do {
+    console.log(index);
+    index++;
+} while (index<=10);
+console.log(`----------------------------------------------------------------`);
+
+// Print 70 to 7 by decrementing each time 7
+ // 70  63  56  
+var index =70;
+do {
+  console.log(index);
+   index = index-7; 
+} while (index>=7);
+console.log(`----------------------------------------------------------------`);
+//WAP to prints numbres from 5 to 15 by incrementing by 1
+//initilization 5 condition i<=15 update expre i++
+var i=5;
+do {
+    console.log(i);
+    i++
+} while (i<=15);
+console.log(`----------------------------------------------------------------`);
+//WAP to print numbers from 50 to 40 by incrementing by 1
+//initilization 50 condition i>=40 update expre i--
+var i=50;
+do {
+    console.log(i);
+    i--
+} while (i>=40);
+console.log(`----------------------------------------------------------------`);
+//WAP to print first 15 odd numbers
+//initilization 1 condition i<=15 update expre i=i+2
+var i=1;
+do {
+    console.log(i);
+    i=i+2
+} while (i<=15);
+console.log(`----------------------------------------------------------------`);
+//WAP to find first 10 even numbers
+//initilization 2 condition i<=20 update expre i=i+2
+var i=2;
+do {
+    console.log(i);
+    i=i+2
+} while (i<=20);
+console.log(`----------------------------------------------------------------`);
+//WAP to print table of 5 like -->5,10,15,---------50
+//initilization 5 condition i<=50 update expre i=i+5
+var i=5;
+do {
+    console.log(i);
+    i=i+5
+} while (i<=50);
+console.log(`----------------------------------------------------------------`);
+//WAP to print table of 10 like --> 10,20,30-------100
+//intilization 10 condition i<=100 update expre i=i+10
+var i=10;
+do {
+    console.log(i);
+    i=i+10
+} while (i<=100);
+console.log(`----------------------------------------------------------------`);
+//WAP to print table of 10 in reverse order like -->100,90,80-------10
+//initilization 100 condition i>=10 update expre i=i-10
+var i=100;
+do {
+    console.log(i);
+    i=i-10
+} while (i>=10);
