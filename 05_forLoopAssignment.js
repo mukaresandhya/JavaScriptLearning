1f377a8100eebf9d559e2dcf7d07112f158a4af8
mukaresandhya <mukaresandhya@gmail.com>
2022-12-21 11:50:52 +0530

    Create 05_forLoopAssignment.js file
    Create 05_forLoopAssignment.js file

---
+++ 05_forLoopAssignment.js
@@ -0,0 +1,45 @@
+
+//Print the numbers from 5 to 15
+for(let index=5;index<=15;index++)
+{
+    console.log(index);
+}
+
+console.log(`--------------------------------------------------------`);
+//Print the numbers from 50 to 40.
+for(let index=50;index>=40;index--)
+{
+    console.log(index);
+}
+console.log(`--------------------------------------------------------`);
+//Print the first 15 odd numbers.
+for(let index=1;index<=15;index=index+2)
+{
+    console.log(index);
+}
+console.log(`--------------------------------------------------------`);
+//WAP to Find first 10 even numbers
+for(let index=2;index<=20;index=index+2)
+{
+    console.log(index);
+}
+console.log(`--------------------------------------------------------`);
+//WAP to print table of 5 like --> 5,10,15--------50
+for(let index=5;index<=50;index=index+5)
+{
+    console.log(index);
+}
+console.log(`--------------------------------------------------------`);
+//WAP to print table of 0 like --> 10,20,30-----100
+for(let index=10;index<=100;index=index+10)
+{
+    console.log(index);
+}
+console.log(`--------------------------------------------------------`);
+//WAP to print table of 10 in reverse order like ----> 100,90,80,------10
+for(let index=100;index>=10;index=index-10)
+{
+    console.log(index);
+}
+
+
