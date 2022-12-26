94f26e271008654814386c9ef1748d434d914090
mukaresandhya <mukaresandhya@gmail.com>
2022-12-26 08:34:55 +0530

    Create 05_breakAndContinue.js file
    Create 05_breakAndContinue.js file

---
+++ 05_breakAndContinue.js
@@ -0,0 +1,17 @@
+
+var index = 0;
+while (index<=10) {
+   console.log(index);// 0 1
+   if (index==5) {
+     break;
+   } 
+   index++;
+}
+
+for (let index = 0; index < 20; index++) {
+   console.log(index);
+   if (index>=12) {
+     break;
+   }
+    
+}
