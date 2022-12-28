3bb7bd7001a0b408cb365269b272d408ef1b8957
mukaresandhya <mukaresandhya@gmail.com>
2022-12-28 12:31:43 +0530

    Update 07_arrayLearning.js file
    Update 07_arrayLearning.js file

--- 07_arrayLearning.js
+++ 07_arrayLearning.js
@@ -125,3 +125,24 @@ arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
 let isAvailable =  arrayOfNum.includes(9);
 console.log(isAvailable);
 
+console.log(`======== join() ==========`);
+arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+let joinResult = arrayOfNum.join(" ");
+console.log(joinResult);
+console.log(typeof joinResult);
+
+console.log(`======== concat() ==========`);
+arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+let arrayOfNames = ["Moni", "Jenny", "Shenny", "Tomy"];
+let arrayOfCities = ["Pune", "Mumbai", "Banglore"];
+const concatArray = arrayOfNum.concat(arrayOfNames, arrayOfCities);
+console.log(concatArray);
+
+console.log(`======== Resize an array ==========`);
+arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+console.log(arrayOfNum.length);
+arrayOfNum.length = 5;
+console.log(arrayOfNum);
+console.log(arrayOfNum.length);
+
+
