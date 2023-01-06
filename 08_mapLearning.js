46e90b4eaddea0b07820db489453c36638d206d0
mukaresandhya <mukaresandhya@gmail.com>
2023-01-06 07:04:10 +0530

    Update 08_mapLearning.js file
    Update 08_mapLearning.js file

--- 08_mapLearning.js
+++ 08_mapLearning.js
@@ -4,6 +4,8 @@ mapOfNumbers.set(22, "Twenty two");
 mapOfNumbers.set(2, "Two");
 mapOfNumbers.set(11, "Ten + One");
 mapOfNumbers.set(33, "Eleven");
+mapOfNumbers.set(44, "Fourty Four");
+mapOfNumbers.set(50, "Fifty");
 console.log(mapOfNumbers);
 
 const valueForKey22 = mapOfNumbers.get(22);
@@ -17,3 +19,17 @@ console.log(mapOfNumbers.size);
 
 mapOfNumbers.delete(11);
 console.log(mapOfNumbers);
+const isKeyAvailable=mapOfNumbers.has(22);
+console.log(isKeyAvailable);
+
+const isKeyAvailables=mapOfNumbers.has(111);
+console.log(isKeyAvailables);
+
+console.log(`-----------------------------Traversing over set----------------------------------`);
+const keyOfMaps=mapOfNumbers.keys();
+console.log(keyOfMaps);
+for (const key of keyOfMaps) 
+{
+    console.log(key,mapOfNumbers.get(key));
+}
+
